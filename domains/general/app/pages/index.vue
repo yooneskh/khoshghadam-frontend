b5003fb65d11b6ce51d2aab179739d69d7b6bd30
Yoones Khoshghadam <yooneskh@gmail.com>
2024-09-18 20:14:51 +0330

    feat (general): tools section in home page

--- domains/general/app/pages/index.vue
+++ domains/general/app/pages/index.vue
@@ -21,17 +21,17 @@ useHead({
         Hello! I am Yoones Khoshghadam.
       </h1>
 
-      <p class="mt-12">
+      <p class="mt-6">
         I am a Full stack developer. <br />
         I can make Web, Android, iOS or Desktop applications.
       </p>
 
-      <p class="mt-12">
+      <p class="mt-6">
         Let's get in touch:
         <a href="mailto:yoones.khoshghadam@gmail.com" class="text-primary underline font-bold">yoones.khoshghadam@gmail.com</a>
       </p>
 
-      <div class="mt-6 space-x-3">
+      <div class="mt-3 space-x-3">
         <nuxt-link to="https://github.com/yooneskh" target="_blank">
           <u-btn
             icon="i-mdi-github"
@@ -44,6 +44,23 @@ useHead({
             label="Twitter"
             class="primary"
           />
+        </nuxt-link>
+      </div>
+
+      <h2 class="mt-12 text-2xl">
+        Tools i made
+      </h2>
+
+      <div class="grid grid-cols-4 mt-6">
+        <nuxt-link to="https://arepo.khoshghadam.com" target="_blank">
+          <u-card class="outline neutral interactive">
+            <h3 class="text-xl font-bold text-center">
+              Arepo
+            </h3>
+            <p class="mt-2">
+              Track who and when opens your emails and ...
+            </p>
+          </u-card>
         </nuxt-link>
       </div>
 
